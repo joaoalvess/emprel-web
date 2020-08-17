ae762797996aeb20d08aef55c4475c815f42690b
Elcoss <jp14700@gmail.com>
2020-08-16 21:40:02 -0300

    add new gitignore

--- src/services/api.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const axios = require('axios').default;
-
-const api = axios.create({
-  baseURL: 'https://emprel-sever.herokuapp.com/',
-});
-
-export default api;
